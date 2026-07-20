11e5577322e18b2d391ba7287da9b4ba7819d4a3
djairofilho <djairodsfilho@gmail.com>
2026-07-20 02:04:44 -0300

    chore: initialize RL benchmark project

---
+++ src/periodic_table_battleship_rl/__init__.py
@@ -0,0 +1,3 @@
+\"\"\"Ambientes e ferramentas de benchmark para Periodic Table Battleship RL.\"\"\"
+
+__version__ = "0.1.0"
---
+++ tests/test_package.py
@@ -0,0 +1,5 @@
+from periodic_table_battleship_rl import __version__
+
+
+def test_version_is_defined() -> None:
+    assert __version__ == "0.1.0"
